ddc84250d35df81a834e10e16cbbc766a7d2c6dd
jean catulle <catullej@wit.edu>
2019-02-19 11:56:51 -0500

    commit2

--- app/src/main/java/com/example/oyotv1/MainActivity.java
+++ app/src/main/java/com/example/oyotv1/MainActivity.java
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             drawer.closeDrawer(GravityCompat.START);
         }else {
             super.onBackPressed();
+
         }
     }
 
